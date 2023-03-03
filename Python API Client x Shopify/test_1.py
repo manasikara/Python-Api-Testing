137d6794cf0180c7cfbaee60d880526f8a7a07bf
manasikara <slawektatinger@me.com>
2023-03-03 16:14:39 +0100

    ....

--- Python API Client x Shopify/test_1.py
+++ Python API Client x Shopify/test_1.py
@@ -1,6 +1,28 @@
-#https://youtu.be/E39a7kQfjSg
+# learning source --> https://youtu.be/E39a7kQfjSg
+
+import requests
+import creds    
+
+def create_session():
+    s = requests.Session()
+    s.headers.update({
+        "X-Shopify-Access-Token": creds.token,
+        "Content-Type": "application/json"
+    })
+    return s
 
 
-
-
-
+def main():
+    sess = create_session()
+    resp = sess.get(creds.url + "/admin/api/2021-07/products.json?limit=10")
+    print(resp.json())
+    
+if __name__ == '__main__':
+    main()    
+    
+    
+    
+    
+    # to be continued - stuck at fricking error "AttributeError: module 'creds' has no attribute 'token'"
+    
+    # 1:52
--- ToDo_API/test_todo_API.py
+++ ToDo_API/test_todo_API.py
@@ -1,6 +1,6 @@
-#PyTest • REST API Integration Testing with Python
-#Learnig material --> https://youtu.be/7dgQRVqF1N0
-#API tested --> https://todo.pixegami.io/
+# PyTest • REST API Integration Testing with Python
+# Learnig material --> https://youtu.be/7dgQRVqF1N0
+# API tested --> https://todo.pixegami.io/
 
 import requests
 
@@ -10,32 +10,31 @@ endpoint = "https://todo.pixegami.io/"
 #WRITING A TEST WITH PYTHON
 response = requests.get(endpoint)
 print(response)
-
 #printing website data
 data = response.json()
 print(data)
-
 #printing the status code
 status_code = response.status_code
 print(status_code)
-
 """
 
-#WRITING A TEST WITH PYTEST
 
-#1st test case - get
+# WRITING A TEST WITH PYTEST
+
+# 1st test case - get
 def test_can_call_endpoint():
     response = requests.get(endpoint)
     assert response.status_code == 200
     pass
 
-#2nd test case - put
+
+# 2nd test case - put
 def test_can_create_task():
     payload = new_task_payload()
     create_task_response = create_task(payload)
     assert create_task_response.status_code == 200
     data = create_task_response.json()
-      
+
     task_id = data['task']['task_id']
     get_task_response = get_task(task_id)
 
@@ -43,16 +42,17 @@ def test_can_create_task():
     get_task_data = get_task_response.json()
     assert get_task_data["content"] == payload["content"]
     assert get_task_data["user_id"] == payload["user_id"]
-    
-#3rd test case - update
+
+
+# 3rd test case - update
 def test_can_update_task():
-    #create a task
+    # create a task
     payload = new_task_payload()
     create_task_response = create_task(payload)
     assert create_task_response.status_code == 200
     task_id = create_task_response.json()["task"]["task_id"]
-    
-    #update the task
+
+    # update the task
     new_payload = {
         "user_id": payload["user_id"],
         "task_id": task_id,
@@ -61,22 +61,38 @@ def test_can_update_task():
     }
     update_task_response = update_task(new_payload)
     assert update_task_response.status_code == 200
-    
-    #get and validate the changes
+
+    # get and validate the changes
     get_task_response = get_task(task_id)
     assert get_task_response.status_code == 200
     get_task_data = get_task_response.json()
     assert get_task_data["content"] == new_payload["content"]
     assert get_task_data["is_done"] == new_payload["is_done"]
-    
+
+def test_can_list_tasks():
+    # Create N tasks.
+    N = 3
+    for _ in range(3):
+        payload = new_task_payload()
+        create_task_response = create_task(payload)
+        assert create_task_response.status_code == 200
+
+    # List tasks, and check that there are N items
+    pass
+
+
+
 def create_task(payload):
     return requests.put(endpoint + "/create-task", json=payload)
+
 
 def update_task(payload):
     return requests.put(endpoint + "/update-task", json=payload)
 
+
 def get_task(task_id):
     return requests.get(endpoint + f"/get-task/{task_id}")
+
 
 def new_task_payload():
     return {
@@ -86,7 +102,4 @@ def new_task_payload():
     }
 
 
-
-
-
-#24:16 - to be continued...
+# 25:13 - to be continued
